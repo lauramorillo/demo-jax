0c3362fca8e202622421a4f4c3e9271396e4260f
Laura Morillo-Velarde <laura.morillovelarde@gmail.com>
2019-10-08 16:22:52 +0100

    Serving a pizza!

--- baseApp/demo/src/main/java/com/example/DemoApp.java
+++ baseApp/demo/src/main/java/com/example/DemoApp.java
@@ -1,9 +1,15 @@
 package com.example;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +19,15 @@ public class DemoApp {
 		SpringApplication.run(DemoApp.class, args);
 	}
 
-    @RequestMapping("/")
-    public String hello(){
-        return "Hello JAX London!";
+    @ResponseBody
+    @RequestMapping(
+            value = "/",
+            method = RequestMethod.GET,
+            produces = MediaType.IMAGE_PNG_VALUE
+    )
+    public ResponseEntity<InputStreamResource> getPizza() {
+        return ResponseEntity.ok()
+                .body(new InputStreamResource(getClass().getResourceAsStream("/static/pizza.png")));
+
     }
 }
